0c4d341f7c4fef064a310f14b4de725b7eef8ce5
rigmunssel <vileee2019@gmail.com>
2026-03-01 01:28:50 +0200

    haha

--- record1.py
+++ record1.py
@@ -5,9 +5,9 @@ from pathlib import Path
 from lerobot.robots.so_follower import SOFollower, SOFollowerRobotConfig
 
 # ── Configuration ────────────────────────────────────────────────────────────
-FOLLOWER_PORT = "/dev/ttyACM0"
+FOLLOWER_PORT = "/dev/ttyACM5"
 FOLLOWER_ID = "follower1"
-LEADER_PORT = "/dev/ttyACM1"
+LEADER_PORT = "/dev/ttyACM4"
 LEADER_ID = "leader1"
 CALIBRATION_DIR = Path(".")
 
